64a0126fab137d99ef8b35e41e98a8988f946419
Kate Lee <kungjin0119@naver.com>
2024-02-02 17:37:06 +0900

    props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,53 @@
-import './App.css';
+import './App.css'; // 이 페이지의 디자인을 수정하는 파일
 
-function App() { // 화면을 구성한는 함수, 파일의 내용을 편집해 가면서 UI를 만든다.
+function Header(props) { // <= 컴포넌트 (사용자 정의 태그)
+  console.log(props)
+  return ( // 리턴값으로 HTML 코드를 반환
+    <header>
+      <h1><a href="/">{props.title}</a></h1> 
+    </header> // 
+  )
+}
+
+function Nav(props) {
+  const lis = []
+  for(let i=0; i<props.topics.length; i++) { // => 아래 <Nav>태그의 왼쪽 topics가 배열객체로 올라옴.
+    let t = props.topics[i]; // 인덱스 0부터 원소의 크기만큼 반복적으로 배열의 객체 값을 저장.
+    lis.push(<li key={t.id}><a href={'/read/'+ t.id}>{t.title}</a></li>);
+  } // => <li><a href="/read/1">html</a></li>
+  console.log({lis})
   return (
-    <div className="App">
-      Hello, React!
+    <nav>
+      <ol> 
+          {lis}
+      </ol>
+    </nav>
+     
+  )
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+       {props.body}
+    </article>
+  )
+}
+
+
+
+function App() { // 화면을 구성하는 함수
+  const topics = [ // 여러 개의 객체를 담은 배열 초기화
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title: 'js', body:'js is...'}
+  ]
+  return ( 
+    <div>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav> 
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,13 +1,16 @@
+// npm start 명령어를 실행하면 Create React App 도구는
+// index.js(입구 파일)를 찾고, 이 안의 코드대로 동작하게 된다.
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // 같은 디렉터리에 있는 App.js로부터 <App /> 태그가 import 됨.
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( // public 폴더의 index.html 파일 참고
   <React.StrictMode>
-    <App />
+    <App /> {/* => UI 전체를 보여주는 태그 */}
   </React.StrictMode>
 );
 
