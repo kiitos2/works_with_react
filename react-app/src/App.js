348fcff07df0acc873522503fefcf4ebf2176381
Kate Lee <kungjin0119@naver.com>
2024-02-06 18:38:21 +0900

    Update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,30 @@
 import './App.css'; // 이 페이지의 디자인을 수정하는 파일
+import {useState} from 'react';
 
 function Header(props) { // <= 컴포넌트 (사용자 정의 태그)
   console.log(props)
   return ( // 리턴값으로 HTML 코드를 반환
     <header>
-      <h1><a href="/">{props.title}</a></h1> 
-    </header> // 
+      <h1><a href="/" onClick={(event)=>{
+        event.preventDefault();
+        props.onChnageMode();
+      }}>{props.title}</a></h1> 
+    </header> 
   )
 }
+
 
 function Nav(props) {
   const lis = []
   for(let i=0; i<props.topics.length; i++) { // => 아래 <Nav>태그의 왼쪽 topics가 배열객체로 올라옴.
-    let t = props.topics[i]; // 인덱스 0부터 원소의 크기만큼 반복적으로 배열의 객체 값을 저장.
-    lis.push(<li key={t.id}><a href={'/read/'+ t.id}>{t.title}</a></li>);
-  } // => <li><a href="/read/1">html</a></li>
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+    <a id={t.id} href={'/read/'+ t.id} onClick={event=>{
+      event.preventDefault();
+      props.onChnageMode(Number(event.target.id));
+    }}>{t.title}</a>
+    </li>);
+  }
   console.log({lis})
   return (
     <nav>
@@ -34,20 +44,125 @@ function Article(props) {
     </article>
   )
 }
+function Create(props) {
+  return (
+    <article>
+        <h2>Create</h2>
+        <form onSubmit={event=>{
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body); 
+        }}>
+          <p><input type="text" name="title" placeholder="title" /></p>
+          <p><textarea name="body" placeholder="body"></textarea></p>
+          <p><input type="submit" value="Create"></input></p>
+        </form>
+    </article>
+  )
+}
 
-
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+        <h2>Update</h2>
+        <form onSubmit={event=>{
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body); 
+        }}>
+          <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+            setTitle(event.target.value);
+          }}/></p>
+          <p><textarea name="body" placeholder="body" value={body} onChange={event=>{
+            setBody(event.target.value);
+          }}></textarea></p>
+          <p><input type="submit" value="Update"></input></p>
+        </form>
+        </article>
+  )
+}
 
 function App() { // 화면을 구성하는 함수
-  const topics = [ // 여러 개의 객체를 담은 배열 초기화
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
-    {id:3, title: 'js', body:'js is...'}
-  ]
+    {id:3, title: 'js', body:'javascript is...'}
+  ]);
+ 
+  let content = null;
+  let contextControl = null;
+  if(mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, Web"></Article>
+  } else if(mode === 'READ') {
+    let title, body =null;
+    for(let i=0; i<topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    } 
+    content = <Article title={title} body={body}></Article>
+    contextControl = <li><a href={"/update/" + id} onClick={event=>{
+      event.preventDefault();
+      setMode('UPDATE');
+    }}>Update</a></li>
+  } else if(mode === 'CREATE') {
+      content = <Create onCreate={(_title, _body)=>{ 
+        const newTopic = {id:nextId, title:_title, body:_body}
+        const newTopics = [...topics]
+        newTopics.push(newTopic);
+        setTopics(newTopics);
+        setMode('READ');
+        setId(nextId);
+        setNextId(nextId+1);
+      }}></Create>
+  } else if(mode === 'UPDATE') {
+    let title, body =null;
+    for(let i=0; i<topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      const newTopics = [...topics];
+      const updateTopic = {id:id, title:title, body:body}
+      for(let i=0; i<newTopics.length; i++) {
+        if(newTopics[i].id === id) {
+          newTopics[i] = updateTopic;
+          break;
+        } 
+      } 
+      setTopics(newTopics);   
+    }}></Update>
+  }
+
+
+
   return ( 
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav> 
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Header title="WEB" onChnageMode={() => {
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChnageMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
+      }}></Nav> 
+      {content}
+      <ul>
+        <li><a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE'); 
+      }}>Create</a></li>
+       {contextControl}
+      </ul>
     </div>
   );
 }
